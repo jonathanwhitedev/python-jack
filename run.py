9c50d8c46f9ab32957529d0e8920ca4ca4ba6db0
jonathanwhitedev <white_jonathan@icloud.com>
2023-03-14 11:33:32 +0000

    created deck class for holding values and suits

--- run.py
+++ run.py
@@ -12,3 +12,35 @@ PYTHONJACK GAME:
 cards.py file for card decoration
 """
 
+
+
+Card = collections.namedtuple('Card', ['value', 'suit'])
+
+class Deck:
+
+    values = [str(v) for v in range(2, 11)] + list('JQKA')
+    suits = "Spades Diamonds Hearts Clubs".split()
+    suit_symbols = ['♠','♦','♥','♣']
+
+    def __init__(self, num_decks=1):
+        self.num_decks = num_decks
+        self.cards = [Card(value, suit) for suit in self.suits for value in self.values] * self.num_decks
+        self.length = len(self)
+
+    def __repr__(self):
+        return "Deck()\n" + ''.join(f"({card.value}-{card.suit})" for card in self.cards)
+
+    def __len__(self):
+        return len(self.cards)
+
+    def __getitem__(self, position):
+        return self.cards[position]
+    
+    def __setitem__(self, position, value):
+        self.cards[position] = value
+
+    def draw_card(self):
+        return self.cards.pop()    
+
+    def shuffle(self):
+        random.shuffle(self.cards)]
